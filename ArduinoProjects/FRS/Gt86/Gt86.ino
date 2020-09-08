220211dc9a12e8a0a329711015c47cc68c7279d2
Jerem <jeremletrol@msn.com>
2020-09-08 23:51:34 +0200

    Fusion between Due and Uno

--- ArduinoProjects/FRS/Gt86/Gt86.ino
+++ ArduinoProjects/FRS/Gt86/Gt86.ino
@@ -1,7 +1,15 @@
 // Includes
+#ifndef __SAM3X8E__
+#include <avr/wdt.h> // Watchdog security
+#endif
 #include "./CANHelperLibrary/Connector/MCPCAN/CMCPCANConnector.h"
 #include "./CANHelperLibrary/Modules/TOYOTA/FRS/CCloseDoors.h"
 #include "./CANHelperLibrary/Modules/TOYOTA/FRS/COilDisplay.h"
+
+#ifdef __SAM3X8E__
+// workaround
+void watchdogSetup(void) {}
+#endif
 
 // Declare the specific connector we want to use
 CAN::CMCPCANConnector S_CAN ;
@@ -16,57 +24,71 @@ FRS::COilDisplay S_OIL_DISPLAY(S_CAN, F_READ_DATA) ;
 /*****************************************************************************/
 void setup()
 {
-	// Wait some time for initialization to be always correct (if powered with ACC or IGN only)
-	DELAY(3000) ;
-	
-	// Open log
-	OPEN_LOG() ;
+  // Wait some time for initialization to be always correct (if powered with ACC or IGN only)
+  DELAY(3000) ;
+  
+  // Open log
+  OPEN_LOG() ;
 
-	// While CAN initialization fails
-	while (!CAN::InitializeCAN(S_CAN, &FRS::FiltersAndMasksConfiguration))
-	{
-		// Tell user
-		PRINTLN_STR("CAN initialization failed") ;
+  // While CAN initialization fails
+  while (!CAN::InitializeCAN(S_CAN, &FRS::FiltersAndMasksConfiguration))
+  {
+    // Tell user
+    PRINTLN_STR("CAN initialization failed") ;
 
-		// Wait some time before trying again
-		DELAY(100) ;
-	}
-	
-	// Ok, everything seems good
-	PRINTLN_STR("CAN initialization OK") ;
+    // Wait some time before trying again
+    DELAY(100) ;
+  }
+  
+  // Ok, everything seems good
+  PRINTLN_STR("CAN initialization OK") ;
+
+#ifdef __SAM3X8E__
+  watchdogEnable(1000);
+#endif
 }
 
 /*****************************************************************************/
 void loop()
-{
-	// Setup the watchdog
-  WDT_Enable(WDT, WDT_MR_WDV(8000)|WDT_MR_WDRSTEN|WDT_MR_WDRPROC);
-	
-	// Update doors lock module
-	S_DOORS_LOCK.Update(MILLIS()) ;
+{  
+#ifndef __SAM3X8E__
+  // Setup the watchdog
+  wdt_enable(WDTO_8S) ;
+#endif
+
+  // Update doors lock module
+  S_DOORS_LOCK.Update(MILLIS()) ;
 
   // Update oil show module
   S_OIL_DISPLAY.Update(MILLIS()) ;
 
-	// No errors occured on CAN ?
-	if (!S_CAN.HasError())
-	{
-		// Reset watchdog
-		WDT_Restart(WDT);
-	}
-	else
-	{
-		// Reset watchdog
-    WDT_Restart(WDT);
-		
-		// Tell user which error it is
-		PRINTLN_STR("Error in CAN, restarting the whole thing") ;
-		PRINTLNHEX(S_CAN.GetError()) ;
-		
-		// Restart in 15ms
-    WDT_Enable(WDT, WDT_MR_WDV(15)|WDT_MR_WDRSTEN|WDT_MR_WDRPROC);
-		
-		// Wait :(
-		while (true) ;
-	}
+  // No errors occured on CAN ?
+  if (!S_CAN.HasError())
+  {
+    // Reset watchdog
+#ifdef __SAM3X8E__
+    watchdogReset();
+#else
+    wdt_reset() ;
+#endif
+  }
+  else
+  {
+#ifndef __SAM3X8E__
+    // Reset watchdog
+    wdt_reset() ;
+#endif
+
+    // Tell user which error it is
+    PRINTLN_STR("Error in CAN, restarting the whole thing") ;
+    PRINTLNHEX(S_CAN.GetError()) ;
+
+#ifndef __SAM3X8E__
+    // Restart in 15ms
+    wdt_enable(WDTO_15MS) ;
+#endif
+    
+    // Wait :(
+    while (true) ;
+  }
 }
